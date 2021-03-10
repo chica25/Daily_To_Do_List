3600457b9dce4d4326d5ffe66dddf7478f0f2042
chica25 <patricia@patriciaco.com>
2021-03-10 02:18:52 -0500

    Implemted filter to task

--- dailyTask.js
+++ dailyTask.js
@@ -15,15 +15,16 @@
 
 // }
     const newForm = document.querySelector('.add')
-    const task = document.querySelector('.task')
+    const list = document.querySelector('.task')
+    const search = document.querySelector('.search input')
 
     const createTemplate = (todoTask) => {
         const html = `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-         <span>${todoTask}</span>
-        <i class="fas fa-trash"></i>`;
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todoTask}</span>
+            <i class="fas fa-trash delete></i>`;
 
-    task.innerHTML += html
+    tasks.innerHTML += html
 }
 
     newForm.addEventListener('submit', e => {
@@ -38,7 +39,25 @@
     });
 
     // delete tasks
-    task.addEventListener('click', e => {
-        if(e.target.classList.contains('delete'))
+    list.addEventListener('click', e => {
+        if(e.target.classList.contains('delete')){
             e.target.parentElement.remove();
+        }
+    });
+
+    const filterTask = (term) => {
+      Array.from(list.children)
+        .filter((task) => {
+            return !task.textContent.includes(term);
+           .forEach(() => {
+               task.classList.add('filtered');
+           })
+        })
+    }
+    
+    search.addEventListener('keyup', () => {
+        const term = search.value.trim();
+        filterTask(term)
     })
+    
+    
