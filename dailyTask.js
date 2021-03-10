be5d808593ec7a21e7271dcea1f8cb21fdbb7b72
chica25 <patricia@patriciaco.com>
2021-03-10 00:43:17 -0500

    edit html

---
+++ dailyTask.js
@@ -0,0 +1,17 @@
+
+
+// music code
+// const song = document.getElementById("song")
+// const icon = document.getElementById("icon")
+
+// icon.onClick = function() {
+//     if(song.paused){
+//         song.play();
+//         icon.src = "images/pause_button.png"
+//     } else {
+//         song.play();
+//         icon.src = "images/play_button.png"
+//     }
+
+// }
+    
